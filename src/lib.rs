2fb8a28ea6dc348eaf0bb19faefafc10f1bae4ad
Tom Pridham <pridham.tom@gmail.com>
2022-03-07 18:56:27 -0700

    feat: add beforeEach macro

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Item};
+
+#[proc_macro_attribute]
+pub fn before_each(_metadata: TokenStream, input: TokenStream) -> TokenStream {
+    let input: Item = match parse_macro_input!(input as Item) {
+        Item::Mod(mut m) => {
+            let (brace, items) = m.content.unwrap();
+            let (before_each_fn, everything_else): (Vec<Item>, Vec<Item>) =
+                items.into_iter().partition(|t| match t {
+                    Item::Fn(f) => f.sig.ident.to_string() == "before_each",
+                    _ => false,
+                });
+            let before_each_fn_block = if before_each_fn.len() != 1 {
+                panic!("The `before_each` macro attribute requires a single function named `before_each` in the body of the module it is called on.")
+            } else {
+                match before_each_fn.into_iter().next().unwrap() {
+                    Item::Fn(f) => f.block,
+                    _ => unreachable!(),
+                }
+            };
+
+            let e: Vec<Item> = everything_else
+                .into_iter()
+                .map(|t| match t {
+                    Item::Fn(mut f) => {
+                        if f.attrs.iter().any(|attr| {
+                            attr.path
+                                .segments
+                                .iter()
+                                // only apply before_each biz to functions with test attributes, this
+                                // includes variants like `tokio::test`, `test_case`
+                                .any(|segment| segment.ident.to_string().contains("test"))
+                        }) {
+                            let mut b = before_each_fn_block.stmts.clone();
+                            b.append(&mut f.block.stmts);
+                            f.block.stmts = b;
+                            Item::Fn(f)
+                        } else {
+                            Item::Fn(f)
+                        }
+                    }
+                    e => e,
+                })
+                .collect();
+            m.content = Some((brace, e));
+            Item::Mod(m)
+        }
+
+        _ => {
+            panic!("The `before_each` macro attribute is only valid when called on a module.")
+        }
+    };
+    TokenStream::from(quote! {#input})
+}
---
+++ tests/helper.rs
@@ -0,0 +1,29 @@
+use test_env_helpers::*;
+
+#[before_each]
+#[cfg(test)]
+mod s {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use test_case::test_case;
+    use tokio;
+
+    static T: AtomicUsize = AtomicUsize::new(0);
+    fn before_each() {
+        T.store(1, Ordering::Relaxed);
+    }
+
+    #[test]
+    fn test_macro() {
+        assert_eq!(T.load(Ordering::SeqCst), 1);
+    }
+    #[tokio::test]
+    async fn async_test_macro() {
+        T.fetch_add(3, Ordering::SeqCst);
+        assert_eq!(T.load(Ordering::SeqCst), 4);
+    }
+    #[test_case(2)]
+    fn async_test_macro_2(_: u8) {
+        T.fetch_add(3, Ordering::SeqCst);
+        assert_eq!(T.load(Ordering::SeqCst), 4);
+    }
+}
